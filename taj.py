50d48a02b0ad485646d540bdc3160c4f50be91e3
vidhi09-tech <vidhi2556@gmail.com>
2020-07-12 11:14:06 +0530

    coding

---
+++ taj.py
@@ -0,0 +1,68 @@
+# the code is so long. take time to read..
+# it takes me 3 hours to complete this coding, as first i drew the image of taj mahal on a piece of paper..
+# i uploaded a snapshot of the output...
+
+
+
+for i in range(1,27):
+    for j in range(1,71):
+        if(i==25 or i==26 or i==24 or j==5 or j==66 or j==4 and i!=1 or j==67 and i!=1 or j==6 and i!=1 or j==65 and
+        i!=1 or j==3 and i!=1 and i!=2 and i!=5 and i!=6 or j==7 and i!=1 and i!=2 and i!=5 and i!=6
+        or j==64  and i!=1 and i!=2 and i!=5 and i!=6 or j==68  and i!=1 and i!=2 and i!=5 and i!=6 or j==2  and
+        i!=1 and i!=2 and i!=3 and i!=5 and i!=6 and i!=8 and i!=9 and i!=10 and i!=11 and i!=12 and i!=13 or j==63
+        and i!=1 and i!=2 and i!=3 and i!=5 and i!=6 and i!=8 and i!=9 and i!=10 and i!=11 and i!=12 and i!=13 or
+        j==8 and i!=1 and i!=2 and i!=3 and i!=5 and i!=6 and i!=8 and i!=9 and i!=10 and i!=11 and i!=12 and i!=13
+        or j==69 and i!=1 and i!=2 and i!=3 and i!=5 and i!=6 and i!=8 and i!=9 and i!=10 and i!=11 and i!=12 and i!=13
+        or i==14 and j==1 or i==14 and j==9 or i==14 and j==62 or i ==14 and j==70 or j==14 and i!=1 and i!=2 and i!=3
+            or j==57 and i!=1 and i!=2 and i!=3 or j==15 and i!=1 and i!=2 and i!=3 and i!=4 or j==56
+         and i != 1 and i != 2 and i != 3 and i!= 4 or j==13 and i!=1 and i!=2 and i!=3 and i!=4 or j==58
+         and i != 1 and i != 2 and i != 3 and i != 4 or j==16 and i!=1 and i!=2 and i!=3 and i!=4 and i!=5 and i!=7 and
+        i!=8 and i!=9 and i!=11 and i!=12 and i!=13 and i!=14 and i!=15 and i!=16 or j==55 and i!=1 and i!=2 and i!=3 and
+        i!=4 and i!=5 and i!=7 and
+        i!=8 and i!=9 and i!=11 and i!=12 and i!=13 and i!=14 and i!=15 and i!=16 or j==12 and i!=1 and i!=2 and i!=3
+        and i!=4 and i!=5 and i!=7 and
+        i!=8 and i!=9 and i!=11 and i!=12 and i!=13 and i!=14 and i!=15 and i!=16 or j==59 and i!=1 and i!=2
+        and i!=3 and i!=4 and i!=5 and i!=7 and
+        i!=8 and i!=9 and i!=11 and i!=12 and i!=13 and i!=14 and i!=15 and i!=16 or j==60 and i==17 or j==11 and i==17
+        or j==17 and i==17 or j==54 and i==17 or j==20 and i!=1 and i!=2 and i!=3 and i!=4 and i!=5 and i !=6 and i!=7
+        and i!=8 or j==21  and i!=1 and i!=2 and i!=3 and i!=4 and i!=5 and i !=6 and i!=7 and i!=8 and i!=9 or j==51
+        and i != 1 and i != 2 and i != 3 and i != 4 and i != 5 and i != 6 and i != 7 and i != 8 or j==50  and i!=1
+        and i!=2 and i!=3 and i!=4 and i!=5 and i !=6 and i!=7 and i!=8 and i!=9 or j==22 and i!=1
+        and i!=2 and i!=3 and i!=4 and i!=5 and i !=6 and i!=7 and i!=8 and i!=9 and i!=16 and i!=17 and i!=18 and i!=21
+        and i!=22 and i!=23 or j==49  and i!=1 and i!=2 and i!=3 and i!=4 and i!=5 and i !=6 and i!=7 and i!=8 and i!=9 and
+        i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23 or j==23  and i!=1 and i!=2 and i!=3 and i!=4 and i!=5
+        and i !=6 and i!=7 and i!=8 and i!=9 and i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23 and i!=15 and i!=20
+        or j==48 and i!=1 and i!=2 and i!=3 and i!=4 and i!=5
+        and i !=6 and i!=7 and i!=8 and i!=9 and i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23 and i!=15 and i!=20
+        or j==24 and i!=1 and i!=2 and i!=3 and i!=4 and i!=5
+        and i !=6 and i!=7 and i!=8 and i!=9 and i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23 or j==47 and i!=1
+        and i!=2 and i!=3 and i!=4 and i!=5
+        and i !=6 and i!=7 and i!=8 and i!=9 and i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23 or j==25
+        and i != 1 and i != 2 and i != 3 and i != 4 and i != 5 and i != 6 and i != 7 and i != 8 and i != 9 and i!=10 and i!=11 or j==46
+        and i != 1 and i != 2 and i != 3 and i != 4 and i != 5 and i != 6 and i != 7 and i != 8 and i != 9 and i != 10 and i != 11
+        or j==26 and i != 1 and i != 2 and i != 3 and i != 4 and i != 5 and i != 6 and i != 7  and i!=10 and i!=11
+        or j==45 and i != 1 and i != 2 and i != 3 and i != 4 and i != 5 and i != 6 and i != 7  and i!=10 and i!=11
+        or j==27 and i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23 and i != 1 and i != 2 and i != 3 and i != 4
+        and i != 5 and i != 6 or j==44  and i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23 and i != 1 and i != 2
+        and i != 3 and i != 4 and i != 5 and i != 6 or j==28 and i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23
+        and i!=15 and i!=20 and i != 1 and i != 2 and i != 3 and i != 4 and i != 5 or j==43 and i!=16 and i!=17 and i!=18
+        and i!=21 and i!=22 and i!=23 and i!=15 and i!=20 and i != 1 and i != 2 and i != 3 and i != 4 and i != 5
+        or j==29 and i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23 and i != 1 and i != 2 and i != 3 and i != 4
+        and i != 5 and i != 6 or j==42 and i!=16 and i!=17 and i!=18 and i!=21 and i!=22 and i!=23 and i != 1 and i != 2
+        and i != 3 and i != 4 and i != 5 and i != 6 or j==30 and i != 1 and i != 2 and i != 3 and i != 4 and i != 5 and
+        i != 6 and i != 7  and i!=10 and i!=11 or j==41 and i != 1 and i != 2 and i != 3 and i != 4 and i != 5 and i != 6
+        and i != 7  and i!=10 and i!=11 or j==32 and i!=1 and i!=2 and i!=3 and i!=6 and i!=7 and i!=10 and i!=11 or
+        j==33 and i!=1 and i!=2 and i!=3 and i!=7 and i!=10 and i!=11 or j==34 and i!=1 and i!=2 and i!=10 and i!=11
+        and i!=17 and i!=18 and i!=19 and i!=20 and i!=21 and i!=22 and i!=23 or j==38  and i!=1 and i!=2 and i!=10 and i!=11
+        and i!=17 and i!=18 and i!=19 and i!=20 and i!=21 and i!=22 and i!=23 or j==39 and i!=1 and i!=2 and i!=3 and i!=7
+        and i!=10 and i!=11 or j==40 and i!=1 and i!=2 and i!=3 and i!=6 and i!=7 and i!=10 and i!=11 or j==35 and i!=1 and
+        i!=10 and i!=11 and i!=16  and i!=17 and i!=18 and i!=19 and i!=20 and i!=21 and i!=22 and i!=23 or j==37
+         and i != 1 and i!= 10 and i != 11 and  i != 16 and i != 17 and i != 18 and i != 19 and i != 20 and i != 21 and i != 22
+        and i != 23 or j==36 and i!=10 and i!=11 and i!=15 and i!=16  and i!=17 and i!=18 and i!=19 and i!=20 and i!=21 and i!=22 and i!=23
+        or j==31 and  i != 1 and i != 2 and i != 3 and i != 4 and i != 5 and i!=6 and i!=7  and i!=10 and i!=11
+
+        ):
+            print("#",end=' ')
+        else:
+            print(" ",end=' ')
+    print()
